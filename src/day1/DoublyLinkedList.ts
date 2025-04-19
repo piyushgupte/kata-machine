63340ffe0a3cdd1a419f7023f3a6ed66a7e9e43e
piyushgupte007 <piyush.gupte@nirmata.com>
2025-04-19 12:03:46 +0530

    doublyLinkList Prepend operation

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,12 +1,29 @@
+type Node<T>={
+    value:T,
+    next?:Node<T>,
+    prev?:Node<T>
+}
 export default class DoublyLinkedList<T> {
     public length: number;
+    public head?:Node<T>;
 
     
 
     constructor() {
+        this.length =0;
+        this.head =undefined;
     }
 
     prepend(item: T): void {
+        const node :Node<T> = {value:item};
+        if(!this.head){
+            this.head=node;
+            return;
+        }
+        node.next=this.head as Node<T>;
+        this.head.prev= node;
+        this.head=node;
+        this.length ++;
 
 }
     insertAt(item: T, idx: number): void {
@@ -16,12 +33,15 @@ export default class DoublyLinkedList<T> {
 
 }
     remove(item: T): T | undefined {
+        return undefined;
 
 }
     get(idx: number): T | undefined {
+        return undefined;
 
 }
     removeAt(idx: number): T | undefined {
+        return undefined;
 
 }
 }
